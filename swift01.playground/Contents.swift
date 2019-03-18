cb792662c157c42ebfa3faf7049c42cf01c92b14
xuanthulabmac <xt2810@gmail.com>
2019-03-18 14:25:27 +0700

    Init

---
+++ swift01.playground/Contents.swift
@@ -0,0 +1,11 @@
+// Lệnh in ra dòng chữ
+print("Hello World!")
+
+//Khai báo một biến
+var yourname = "XuanThuLab"
+
+/* Lệnh print có thể in các biến
+ và có thể chèn giá trị biến vào chuỗi bằng ký hiệu \(tenbien)*/
+
+print(yourname)
+print("Xin chào \(yourname)")
