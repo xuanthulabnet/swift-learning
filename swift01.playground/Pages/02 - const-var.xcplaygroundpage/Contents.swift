8ace03fa9f5a4f637bcc00610e88bc1ce4a6d56d
xuanthulabmac <xt2810@gmail.com>
2019-03-18 19:13:57 +0700

    U

---
+++ swift01.playground/Pages/02 - const-var.xcplaygroundpage/Contents.swift
@@ -0,0 +1,21 @@
+//Khai báo biến với từ khóa var
+//kiểu của biến sẽ xác định theo giá trị khởi tạo của nó
+//Ví dụ gán là 30, nên student_age sẽ là kiểu số nguyên
+var student_age = 30;
+print("Tuổi là: \(student_age)")
+
+//Biến có thể gán giá trị khác bất kỳ đâu
+student_age = 30;
+print("Tuổi là: \(student_age)")
+
+//Có thể khai báo nhiều biến trên một dòng, cách nhau bởi dấu ,
+var student_name = "XuanThuLab", student_weight = 60.8
+print("Tên: \(student_name), Cân nặng: \(student_weight), Tuổi: \(student_age)")
+
+//Khai báo hằng số dùng khóa let
+let months_of_year = 12
+/* months_of_year khai báo là hằng số,
+ nếu cố tình thay đổi giá trị sau khi nó khởi tạo ví dụ
+ months_of_year = 11, sẽ gây ra lỗi */
+
+print("Một năm có \(months_of_year) tháng")
